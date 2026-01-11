9632f626aace831eb0c28612bda18504e3f2f60d
PrabeshChalise <112958999+PrabeshChalise@users.noreply.github.com>
2026-01-11 12:46:00 +0545

    Delete backend directory

--- backend/config/cloudinary.js
+++
@@ -1,9 +0,0 @@
-import { v2 as cloudinary } from "cloudinary";
-
-cloudinary.config({
-  cloud_name: "dsnmhnj0b",
-  api_key: "812327346814326",
-  api_secret: "35duA2Z6IV4sNVzcOIsZVsrffrQ",
-});
-
-export default cloudinary;
